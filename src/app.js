0e45aec0f089ce9bc1fed476ba9e4e4cec75a2ec
Vikas <vikasakula@Vikass-MacBook-Air.local>
2026-07-10 13:48:39 +0530

    Add dashboard UI

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const webhookRoutes = require('./routes/webhookRoutes');
 const app = express();
+const path = require('path'); 
 app.use(express.json({ limit: '1mb' }));
 
 // Serve static files
